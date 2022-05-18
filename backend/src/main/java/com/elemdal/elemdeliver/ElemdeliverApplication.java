bd991fdade8845e7e754a18427fa860f56b5afdb
AnaCristina1972 <anac.s.vieira@hotmail.com>
2022-05-18 19:32:46 -0400

    Project created

---
+++ backend/src/main/java/com/elemdal/elemdeliver/ElemdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.elemdal.elemdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElemdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElemdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/elemdal/elemdeliver/ElemdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elemdal.elemdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElemdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
